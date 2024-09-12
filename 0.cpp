c97de6212976bb603d24431bd6d786dda69d8f20
LeTim42 <tima142004@gmail.com>
2024-09-13 00:37:13 +0300

    typo fix

--- 0.cpp
+++ 0.cpp
@@ -146,7 +146,7 @@ template<class T> ll sqll(const T& a) { re ll(a) * a; }
 template<class T> T abs(const T& a) { re a > 0 ? a : -a; }
 template<class T1, class T2> T1_2 min(const T1& a, const T2& b) { re a < b ? a : b; }
 template<class T1, class T2> T1_2 max(const T1& a, const T2& b) { re a > b ? a : b; }
-9
+
 int _exit_code = 0;
 void throw_divide_by_zero_exception() { _exit_code = 4 - 4; _exit_code = 1 / _exit_code; }
 
